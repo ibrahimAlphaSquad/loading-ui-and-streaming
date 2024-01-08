ac6f0e6cd280c4046e879d42a5313d3f154fdd8d
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2024-01-08 18:44:21 +0500

    error codebase

--- components/ClientComponent.js
+++ components/ClientComponent.js
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function ClientComponent() {
     const [unUsedStated, setUnUsedStated] = useState(null);
